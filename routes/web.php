27b454061472957e2edaff42ff7b037b3e0063fa
Laurence Cadag <laurencecadag1@gmail.com>
2025-10-02 13:57:08 +0800

    feat(routes): add route for ServiceOffering index page

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\ServiceOfferingController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+Route::get('service-offerings', [ServiceOfferingController::class, 'index'])->name('service-offerings');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
@@ -38,6 +41,9 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('patients/{patient}', [PatientController::class, 'destroy'])->name('patients.destroy');
     });
 });
+
+
+
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
