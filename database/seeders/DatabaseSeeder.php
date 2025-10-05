27187dbd54443f6b5fcfa346b6d185bf2f47c638
Laurence Cadag <laurencecadag1@gmail.com>
2025-10-05 14:16:18 +0800

    feat(database-seeder): reorganize seeder calls and remove commented user factory code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,29 +2,22 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\PermissionSeeder;
 use Database\Seeders\RoleSeeder;
+use Database\Seeders\PatientSeeder;
+use Database\Seeders\ServiceOfferingSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
+            ServiceOfferingSeeder::class,
+            PatientSeeder::class,
         ]);
-
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
